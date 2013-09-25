1feb44a5822586ec3dd533ade734c9b9fa0c438d
Michael Sevestre <michael.sevestre@gmail.com>
2013-09-25 16:22:42 -0400

    Remove file for attachment not supported by ckeditor

--- lib/monologue/image_upload_configuration.rb
+++ lib/monologue/image_upload_configuration.rb
@@ -1,10 +1,9 @@
 module Monologue
   class ImageUploadConfiguration
-    attr_accessor :max_attachment_size, :max_picture_size, :upload_path
+    attr_accessor :max_picture_size, :upload_path
 
     def initialize
-      self.max_attachment_size = 100.megabytes
-      self.max_picture_size = 2.megabytes
+      self.max_picture_size = 1.megabytes
       self.upload_path = "monologue"
     end
   end
--- spec/dummy/config/initializers/monologue.rb
+++ spec/dummy/config/initializers/monologue.rb
@@ -1,5 +1,4 @@
-Monologue.config do |config|
-  config.image_upload.max_attachment_size = 50.megabytes
-  config.image_upload.max_picture_size = 1.megabytes
-  config.image_upload.upload_path = "monologue"
+Monologue.config do |monologue|
+  monologue.image_upload.max_picture_size = 100.kilobytes
+  monologue.image_upload.upload_path = "monologue"
 end
