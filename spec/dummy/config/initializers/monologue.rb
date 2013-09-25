4bc091a41f57c41f96990513b7d018cc628332be
Michael Sevestre <michael.sevestre@gmail.com>
2013-09-25 16:25:55 -0400

    Change default for max picture size in dummy app

--- spec/dummy/config/initializers/monologue.rb
+++ spec/dummy/config/initializers/monologue.rb
@@ -1,4 +1,4 @@
 Monologue.config do |monologue|
-  monologue.image_upload.max_picture_size = 100.kilobytes
+  monologue.image_upload.max_picture_size = 2.megabytes
   monologue.image_upload.upload_path = "monologue"
 end
