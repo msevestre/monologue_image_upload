935df403f17f1735cd86e5f6343d02928b0c8961
Michael Sevestre <michael.sevestre@gmail.com>
2013-09-25 16:37:21 -0400

    Update documentation and removed route that is not required

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 MonologueImageUpload::Engine.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   mount Monologue::Engine, :at => "/monologue"
   mount Ckeditor::Engine => '/ckeditor'
 end
